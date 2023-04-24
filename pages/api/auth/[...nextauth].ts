44107311a7a716fb62be603bb27f16bf6e9f621b
frnk26 <frankahlennn@gmail.com>
2023-04-24 12:12:35 +0800

    create

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,7 +5,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import Stripe from 'stripe'
 const prisma = new PrismaClient()
 
-export default NextAuth({
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -32,4 +32,6 @@ export default NextAuth({
       }
     }
   }
-})
+}
+
+export default NextAuth(authOptions)
